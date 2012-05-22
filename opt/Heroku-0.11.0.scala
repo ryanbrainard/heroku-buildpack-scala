3bd22c25aed144d5e33302c40068c9886dff3300
Ryan Brainard <brainard@heroku.com>
2012-05-21 20:49:38 -0700

    fix ref to maven-sonatype-oss-scala-tools

--- opt/Heroku-0.11.0.scala
+++ opt/Heroku-0.11.0.scala
@@ -7,7 +7,7 @@ object Heroku extends Plugin {
       Seq(Resolver.defaultLocal) ++ appResolvers ++
       Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://s3pository-staging-brainard.herokuapp.com/ivy-typesafe-releases/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
           "heroku-central" at "http://s3pository-staging-brainard.herokuapp.com/maven-central/",
-          "heroku-scala-tools-releases" at "http://s3pository-staging-brainard.herokuapp.com/maven-scala-tools-releases/",
+          "heroku-scala-tools-releases" at "http://s3pository-staging-brainard.herokuapp.com/maven-sonatype-oss-scala-tools/",
           "heroku-scala-tools-snapshots" at "http://s3pository-staging-brainard.herokuapp.com/maven-scala-tools-snapshots/")
   })
 }
